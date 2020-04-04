e7395a835ab2038a6f2190a915b14aa3e3a2680b
Krishna Lahoti <krishlahoti@gmail.com>
2020-04-04 14:16:29 +0530

    Refactoring sendMessage.js file

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import textarea from '@material-ui/core';
 import './App.css';
-import ReactFileReader from 'react-file-reader';
 import CSVReader from 'react-csv-reader'
 
 export default class SendMessage extends React.Component {
@@ -12,52 +11,48 @@ export default class SendMessage extends React.Component {
         super(props);
         this.state = {
             value: '',
-            phoneNumber:''
+            phoneNumber: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.sendMessage = this.sendMessage.bind(this);
-        this.handleFiles=this.handleFiles.bind(this);
+        this.handleFiles = this.handleFiles.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        this.setState({value: event.target.value});
     }
 
-    handleFiles(data,fileInfo){
-        var temp='';
-        for(let i=1;i<data.length-1;i++){
-            if(i==data.length-2){
-                temp=temp+'91'+data[i][1];
-            }
-            else{
-                temp=temp+'91'+data[i][1]+',';
+    handleFiles(data, fileInfo) {
+        let temp = '';
+        for (let i = 1; i < data.length - 1; i++) {
+            if (i === data.length - 2) {
+                temp = temp + '91' + data[i][1];
+            } else {
+                temp = temp + '91' + data[i][1] + ',';
             }
         }
-        this.setState({phoneNumber:temp});
+        this.setState({phoneNumber: temp});
         console.log(this.state.phoneNumber);
     }
 
     async sendMessage() {
-        for (let i = 0; i < this.state.result.length; i++) {
-            console.log(this.state.result[i]);
-        }
-        var params = {
-            username: 'nileshrathi2dec@gmail.com',
-            hash: '44393bb2eb8fc745359b554afda35df29fbda0dc6566a9412cf1ba4ff6390592',
+        let params = {
+            username: 'krishna.lahoti96@gmail.com',
+            hash: '6e32093a03fc522ddf340140e073127562a23897fcf5175146a6765994f65320',
             message: this.state.value,
             sender: 'TXTLCL',
             numbers: this.state.phoneNumber,
             test: '0'
         };
 
-        var formData = new FormData();
+        let formData = new FormData();
 
-        for (var k in params) {
+        for (let k in params) {
             formData.append(k, params[k]);
         }
 
-        var request = {
+        let request = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -75,15 +70,12 @@ export default class SendMessage extends React.Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <textarea className="send-msg" value={this.state.value} onChange={this.handleChange} />
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <textarea className="send-msg" value={this.state.value} onChange={this.handleChange}/>
                     <Button className="btn-snd-msg" variant="contained" color="primary" onClick={this.sendMessage}>
                         Send Message
                     </Button>
-                    {/* <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
-                        <button className='btn'>Upload</button>
-                    </ReactFileReader> */}
-                    <CSVReader onFileLoaded={(data, fileInfo) => this.handleFiles(data, fileInfo)} />
+                    <CSVReader onFileLoaded={(data, fileInfo) => this.handleFiles(data, fileInfo)}/>
                 </header>
             </div>
         );
